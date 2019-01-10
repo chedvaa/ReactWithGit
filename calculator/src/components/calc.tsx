4a062eedab508e2eed4e2ce2a6a0ee1bf69dbbb9
Chaya Farkash <farkashchaya@gmail.com>
2019-01-10 09:35:15 +0200

    add index.html

--- calculator/src/components/calc.tsx
+++ calculator/src/components/calc.tsx
@@ -1,19 +1,57 @@
 import *as React from 'react';
-import {connect} from "react-redux";
-import {Dispatch} from "redux";
+// import {connect} from "react-redux";
+// import {Dispatch} from "redux";
+// import {number} from "prop-types";
 
-class CalcComponent extends React.Component {
+interface Istate {
+    math:string;
+}
 
+class CalcComponent extends React.Component<any,Istate> {
+
+    constructor(props:any){
+        super(props);
+        this.state={math:""};
+        this.change=this.change.bind(this);
+    }
+
+    public change(e:any){
+        this.setState({math:this.state.math+" "+e.target.value});
+    }
 
     public render() {
         return (
             <div>
-                <input/>&nbsp<input/>
+                <input id={"num"}>{this.state.math}</input>
                 <br/><br/>
-                <input type={"button"} value={"+"} onClick={}/>&nbsp
-                <input type={"button"} value={"-"} onClick={}/>&nbsp
-                <input type={"button"} value={"/"} onClick={}/>&nbsp
-                <input type={"button"} value={"*"} onClick={}/>&nbsp
+                <table>
+                    <tr>
+                        <td><input type={"button"} value={"7"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"8"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"9"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"/"} onClick={this.change}/></td>
+                    </tr>
+                    <tr>
+                        <td><input type={"button"} value={"4"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"5"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"6"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"*"} onClick={this.change}/></td>
+                    </tr>
+                    <tr>
+                        <td><input type={"button"} value={"1"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"2"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"3"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"-"} onClick={this.change}/></td>
+                    </tr>
+                    <tr>
+                        <td><input type={"button"} value={"0"} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"="} onClick={this.change}/></td>
+                        <td><input type={"button"} value={"+"} onClick={this.change}/></td>
+                    </tr>
+                </table>
+
+
+
             </div>
         );
     }
@@ -21,15 +59,16 @@ class CalcComponent extends React.Component {
 
 }
 
-const mapStateToProps = (state: any) => {
-      return null;
-}
+// const mapStateToProps = (state: any) => {
+//       return null;
+// };
 
-const mapDispatchToProps = (dispatch: Dispatch) => {
+// const mapDispatchToProps = (dispatch: Dispatch) => {
+//
+// };
 
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(CalcComponent);
+export default CalcComponent;
+// export default connect(mapStateToProps,mapDispatchToProps)(CalcComponent);
 
 
 
