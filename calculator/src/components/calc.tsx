e3a3f191103044a167d5e2178a856523df8d1a2a
Leah Aiznberg <leah14155@gmail.com>
2019-01-10 08:26:56 +0200

    add component calc

---
+++ calculator/src/components/calc.tsx
@@ -0,0 +1,36 @@
+import *as React from 'react';
+import {connect} from "react-redux";
+import {Dispatch} from "redux";
+
+class CalcComponent extends React.Component {
+
+
+    public render() {
+        return (
+            <div>
+                <input/>&nbsp<input/>
+                <br/><br/>
+                <input type={"button"} value={"+"}/>&nbsp
+                <input type={"button"} value={"-"}/>&nbsp
+                <input type={"button"} value={"/"}/>&nbsp
+                <input type={"button"} value={"*"}/>&nbsp
+            </div>
+        );
+    }
+
+
+}
+
+const mapStateToProps = (state: any) => {
+      return null;
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CalcComponent);
+
+
+
+
