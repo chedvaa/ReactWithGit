3c996999a69f069181974f703c66fd456c7fa9dc
gili eschwege <gily3969@gmail.com>
2019-01-13 12:22:37 +0200

    all functions

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,35 +1,47 @@
-// import {types}from './actionTypes'
-// import {Action, handleActions, Reducer} from "redux-actions";
-//
-// interface ICalc {
-//     result:number;
-// }
-//
-// const initialSTATE:ICalc={
-//     result:0
-// }
-//
-// export const mainReducer: Reducer<ICalc, any> = handleActions<ICalc, any>({
-//
-//     [types.ADD_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
-//         return {
-//             ...state, result: {...(action.payload.Num1+action.payload.Num2)}
-//         };
-//     },
-//     [types.SUB_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
-//         return {
-//             ...state, result: {...(action.payload.Num1-action.payload.Num2)}
-//         };
-//     },
-//     [types.MUL_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
-//         return {
-//             ...state, result: {...(action.payload.Num1+action.payload.Num2)}
-//         };
-//     },
-//     [types.DIV_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
-//         return {
-//             ...state, result: {...(action.payload.Num1+action.payload.Num2)}
-//         };
-//     },
-// }, initialSTATE);
-// export default mainReducer;
+import {types}from './actionTypes'
+import {Action, handleActions, Reducer} from "redux-actions";
+
+interface ICalc {
+    result:number;
+}
+
+const initialSTATE:ICalc={
+    result:0
+}
+
+export const mainReducer: Reducer<ICalc, any> = handleActions<ICalc, any>({
+
+    [types.ADD_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
+        const num1:number= +action.payload.Num1;
+        const num2:number= +action.payload.Num2;
+        const h:any=num1+num2;
+        return {
+            ...state, result: {...h}
+        };
+    },
+    [types.SUB_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
+       const num1:number= +action.payload.Num1;
+       const num2:number= +action.payload.Num2;
+       const h:any=num1-num2;
+        return {
+            ...state, result: {...h}
+        };
+    },
+    [types.MUL_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
+        const num1:number= +action.payload.Num1;
+        const num2:number= +action.payload.Num2;
+        const h:any=num1*num2;
+        return {
+            ...state, result: {...h}
+        };
+    },
+    [types.DIV_RESULT]: (state: ICalc = initialSTATE, action: Action<any>): ICalc => {
+        const num1:number= +action.payload.Num1;
+        const num2:number= +action.payload.Num2;
+        const h:any=num1/num2;
+        return {
+            ...state, result: {...h}
+        };
+    },
+}, initialSTATE);
+export default mainReducer;
